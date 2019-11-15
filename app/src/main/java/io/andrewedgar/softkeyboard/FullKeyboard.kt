b58b55e1698d467af993f69395d6d09973d6be5b
Andrew Edgar <andrew@andrewedgar.io>
2019-11-15 13:50:56 -0500

    Initial Commit

--- app/src/main/java/io/andrewedgar/softkeyboard/FullKeyboard.kt
+++ app/src/main/java/io/andrewedgar/softkeyboard/FullKeyboard.kt
@@ -7,52 +7,13 @@ import android.util.SparseArray
 import android.view.LayoutInflater
 import android.view.View
 import android.view.inputmethod.InputConnection
-import android.widget.Button
 import android.widget.LinearLayout
-
+import kotlinx.android.synthetic.main.keyboard.view.*
 
 
 class FullKeyboard @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : LinearLayout(context, attrs, defStyleAttr), View.OnClickListener {
-
-    private lateinit var mButton1: Button
-    private lateinit var mButton2: Button
-    private lateinit var mButton3: Button
-    private lateinit var mButton4: Button
-    private lateinit var mButton5: Button
-    private lateinit var mButton6: Button
-    private lateinit var mButton7: Button
-    private lateinit var mButton8: Button
-    private lateinit var mButton9: Button
-    private lateinit var mButton0: Button
-    private lateinit var mButtonQ: Button
-    private lateinit var mButtonW: Button
-    private lateinit var mButtonE: Button
-    private lateinit var mButtonR: Button
-    private lateinit var mButtonT: Button
-    private lateinit var mButtonY: Button
-    private lateinit var mButtonU: Button
-    private lateinit var mButtonI: Button
-    private lateinit var mButtonO: Button
-    private lateinit var mButtonP: Button
-    private lateinit var mButtonA: Button
-    private lateinit var mButtonS: Button
-    private lateinit var mButtonD: Button
-    private lateinit var mButtonF: Button
-    private lateinit var mButtonG: Button
-    private lateinit var mButtonH: Button
-    private lateinit var mButtonJ: Button
-    private lateinit var mButtonK: Button
-    private lateinit var mButtonL: Button
-    private lateinit var mButtonZ: Button
-    private lateinit var mButtonX: Button
-    private lateinit var mButtonC: Button
-    private lateinit var mButtonV: Button
-    private lateinit var mButtonB: Button
-    private lateinit var mButtonN: Button
-    private lateinit var mButtonM: Button
-    private lateinit var mButtonDelete: Button
 
     var keyValues = SparseArray<String>(36)
 
@@ -63,122 +24,82 @@ class FullKeyboard @JvmOverloads constructor(
 
         LayoutInflater.from(context).inflate(R.layout.keyboard, this, true)
 
-
-        mButton1 = findViewById(R.id.button_1)
-        mButton2 = findViewById(R.id.button_2)
-        mButton3 = findViewById(R.id.button_3)
-        mButton4 = findViewById(R.id.button_4)
-        mButton5 = findViewById(R.id.button_5)
-        mButton6 = findViewById(R.id.button_6)
-        mButton7 = findViewById(R.id.button_7)
-        mButton8 = findViewById(R.id.button_8)
-        mButton9 = findViewById(R.id.button_9)
-        mButton0 = findViewById(R.id.button_0)
-        mButtonQ = findViewById(R.id.button_Q)
-        mButtonW = findViewById(R.id.button_W)
-        mButtonE = findViewById(R.id.button_E)
-        mButtonR = findViewById(R.id.button_R)
-        mButtonT = findViewById(R.id.button_T)
-        mButtonY = findViewById(R.id.button_Y)
-        mButtonU = findViewById(R.id.button_U)
-        mButtonI = findViewById(R.id.button_I)
-        mButtonO = findViewById(R.id.button_O)
-        mButtonP = findViewById(R.id.button_P)
-        mButtonA = findViewById(R.id.button_A)
-        mButtonS = findViewById(R.id.button_S)
-        mButtonD = findViewById(R.id.button_D)
-        mButtonF = findViewById(R.id.button_F)
-        mButtonG = findViewById(R.id.button_G)
-        mButtonH = findViewById(R.id.button_H)
-        mButtonJ = findViewById(R.id.button_J)
-        mButtonK = findViewById(R.id.button_K)
-        mButtonL = findViewById(R.id.button_L)
-        mButtonZ = findViewById(R.id.button_Z)
-        mButtonX = findViewById(R.id.button_X)
-        mButtonC = findViewById(R.id.button_C)
-        mButtonV = findViewById(R.id.button_V)
-        mButtonB = findViewById(R.id.button_B)
-        mButtonN = findViewById(R.id.button_N)
-        mButtonM = findViewById(R.id.button_M)
-        mButtonDelete = findViewById(R.id.button_delete)
-
-
-        mButton1.setOnClickListener(this)
-        mButton2.setOnClickListener(this)
-        mButton3.setOnClickListener(this)
-        mButton4.setOnClickListener(this)
-        mButton5.setOnClickListener(this)
-        mButton6.setOnClickListener(this)
-        mButton7.setOnClickListener(this)
-        mButton8.setOnClickListener(this)
-        mButton9.setOnClickListener(this)
-        mButton0.setOnClickListener(this)
-        mButtonQ.setOnClickListener(this)
-        mButtonW.setOnClickListener(this)
-        mButtonE.setOnClickListener(this)
-        mButtonR.setOnClickListener(this)
-        mButtonT.setOnClickListener(this)
-        mButtonY.setOnClickListener(this)
-        mButtonU.setOnClickListener(this)
-        mButtonI.setOnClickListener(this)
-        mButtonO.setOnClickListener(this)
-        mButtonP.setOnClickListener(this)
-        mButtonA.setOnClickListener(this)
-        mButtonS.setOnClickListener(this)
-        mButtonD.setOnClickListener(this)
-        mButtonF.setOnClickListener(this)
-        mButtonG.setOnClickListener(this)
-        mButtonH.setOnClickListener(this)
-        mButtonJ.setOnClickListener(this)
-        mButtonK.setOnClickListener(this)
-        mButtonL.setOnClickListener(this)
-        mButtonZ.setOnClickListener(this)
-        mButtonX.setOnClickListener(this)
-        mButtonC.setOnClickListener(this)
-        mButtonV.setOnClickListener(this)
-        mButtonB.setOnClickListener(this)
-        mButtonN.setOnClickListener(this)
-        mButtonM.setOnClickListener(this)
-        mButtonDelete.setOnClickListener(this)
+        button1.setOnClickListener(this)
+        button2.setOnClickListener(this)
+        button3.setOnClickListener(this)
+        button4.setOnClickListener(this)
+        button5.setOnClickListener(this)
+        button6.setOnClickListener(this)
+        button7.setOnClickListener(this)
+        button8.setOnClickListener(this)
+        button9.setOnClickListener(this)
+        button0.setOnClickListener(this)
+        buttonQ.setOnClickListener(this)
+        buttonW.setOnClickListener(this)
+        buttonE.setOnClickListener(this)
+        buttonR.setOnClickListener(this)
+        buttonT.setOnClickListener(this)
+        buttonY.setOnClickListener(this)
+        buttonU.setOnClickListener(this)
+        buttonI.setOnClickListener(this)
+        buttonO.setOnClickListener(this)
+        buttonP.setOnClickListener(this)
+        buttonA.setOnClickListener(this)
+        buttonS.setOnClickListener(this)
+        buttonD.setOnClickListener(this)
+        buttonF.setOnClickListener(this)
+        buttonG.setOnClickListener(this)
+        buttonH.setOnClickListener(this)
+        buttonJ.setOnClickListener(this)
+        buttonK.setOnClickListener(this)
+        buttonL.setOnClickListener(this)
+        buttonZ.setOnClickListener(this)
+        buttonX.setOnClickListener(this)
+        buttonC.setOnClickListener(this)
+        buttonV.setOnClickListener(this)
+        buttonB.setOnClickListener(this)
+        buttonN.setOnClickListener(this)
+        buttonM.setOnClickListener(this)
+        buttonDelete.setOnClickListener(this)
 
 
 
-        keyValues.put(R.id.button_1, "1")
-        keyValues.put(R.id.button_2, "2")
-        keyValues.put(R.id.button_3, "3")
-        keyValues.put(R.id.button_4, "4")
-        keyValues.put(R.id.button_5, "5")
-        keyValues.put(R.id.button_6, "6")
-        keyValues.put(R.id.button_7, "7")
-        keyValues.put(R.id.button_8, "8")
-        keyValues.put(R.id.button_9, "9")
-        keyValues.put(R.id.button_0, "0")
-        keyValues.put(R.id.button_Q, "Q")
-        keyValues.put(R.id.button_W, "W")
-        keyValues.put(R.id.button_E, "E")
-        keyValues.put(R.id.button_R, "R")
-        keyValues.put(R.id.button_T, "T")
-        keyValues.put(R.id.button_Y, "Y")
-        keyValues.put(R.id.button_U, "U")
-        keyValues.put(R.id.button_I, "I")
-        keyValues.put(R.id.button_O, "O")
-        keyValues.put(R.id.button_P, "P")
-        keyValues.put(R.id.button_A, "A")
-        keyValues.put(R.id.button_S, "S")
-        keyValues.put(R.id.button_D, "D")
-        keyValues.put(R.id.button_F, "F")
-        keyValues.put(R.id.button_G, "G")
-        keyValues.put(R.id.button_H, "H")
-        keyValues.put(R.id.button_J, "J")
-        keyValues.put(R.id.button_K, "K")
-        keyValues.put(R.id.button_L, "L")
-        keyValues.put(R.id.button_Z, "Z")
-        keyValues.put(R.id.button_X, "X")
-        keyValues.put(R.id.button_C, "C")
-        keyValues.put(R.id.button_V, "V")
-        keyValues.put(R.id.button_B, "B")
-        keyValues.put(R.id.button_N, "N")
-        keyValues.put(R.id.button_M, "M")
+        keyValues.put(R.id.button1, "1")
+        keyValues.put(R.id.button2, "2")
+        keyValues.put(R.id.button3, "3")
+        keyValues.put(R.id.button4, "4")
+        keyValues.put(R.id.button5, "5")
+        keyValues.put(R.id.button6, "6")
+        keyValues.put(R.id.button7, "7")
+        keyValues.put(R.id.button8, "8")
+        keyValues.put(R.id.button9, "9")
+        keyValues.put(R.id.button0, "0")
+        keyValues.put(R.id.buttonQ, "Q")
+        keyValues.put(R.id.buttonW, "W")
+        keyValues.put(R.id.buttonE, "E")
+        keyValues.put(R.id.buttonR, "R")
+        keyValues.put(R.id.buttonT, "T")
+        keyValues.put(R.id.buttonY, "Y")
+        keyValues.put(R.id.buttonU, "U")
+        keyValues.put(R.id.buttonI, "I")
+        keyValues.put(R.id.buttonO, "O")
+        keyValues.put(R.id.buttonP, "P")
+        keyValues.put(R.id.buttonA, "A")
+        keyValues.put(R.id.buttonS, "S")
+        keyValues.put(R.id.buttonD, "D")
+        keyValues.put(R.id.buttonF, "F")
+        keyValues.put(R.id.buttonG, "G")
+        keyValues.put(R.id.buttonH, "H")
+        keyValues.put(R.id.buttonJ, "J")
+        keyValues.put(R.id.buttonK, "K")
+        keyValues.put(R.id.buttonL, "L")
+        keyValues.put(R.id.buttonZ, "Z")
+        keyValues.put(R.id.buttonX, "X")
+        keyValues.put(R.id.buttonC, "C")
+        keyValues.put(R.id.buttonV, "V")
+        keyValues.put(R.id.buttonB, "B")
+        keyValues.put(R.id.buttonN, "N")
+        keyValues.put(R.id.buttonM, "M")
 
 
     }
@@ -188,7 +109,7 @@ class FullKeyboard @JvmOverloads constructor(
 
 
 
-        if (v?.id == R.id.button_delete) {
+        if (v?.id == R.id.buttonDelete) {
             val selectedText: CharSequence? = inputConnection.getSelectedText(0)
 
             if (TextUtils.isEmpty(selectedText)) {
